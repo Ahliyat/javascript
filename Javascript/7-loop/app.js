32dac48a9cf66cc7f6ff0159470e675b00df4711
Ahliyat <145200900+Ahliyat@users.noreply.github.com>
2023-12-17 11:21:02 +0100

    Add files via upload

---
+++ Javascript/7-loop/app.js
@@ -0,0 +1,22 @@
+/**
+ * manually counting 1-10
+ */
+console.log("counting manually 1-10" )
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log(6);
+console.log(7);
+console.log(8);
+console.log(9);
+console.log(10);
+
+
+/**
+ * counting 1-10 using "for loop"
+ */
+for (let i = 1; i <= 10; i++) {
+ console.log("using for loop to count 1-10" ,i)
+}
---
+++ Javascript/functions/app.js
@@ -0,0 +1,23 @@
+/**
+ * Question 1
+ * Create a function named welcomehome that will take name as parameter and return
+ * the string 'welcome back home ${name}'
+ */
+
+/** solution */
+function welcomehome(Ahliyat) {
+  return 'welcome back home '
+}
+
+/**
+ * Question 2
+ * write a function called volumeofsphere to claculate the volume of a sphere 
+ * volume = 4/3πr3
+ */
+/**
+ * Question 3
+ * write a function named favoritecitythat will
+ * takes your name and favorite city as parameters and
+ * will return the strings 'my name is ${name} and favorite city is ${city}'
+ */
+
